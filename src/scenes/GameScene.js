b529aed3bdf7627c5fecf2a88eb87013bd28a108
vero-code <79207983+vero-code@users.noreply.github.com>
2025-11-17 19:44:53 +0200

    feat: add keyboard movement

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -16,10 +16,34 @@ export default class GameScene extends Phaser.Scene {
     this.layers.ground = this.map.createLayer("Dungeon", tileset, 0, 0);
     this.layers.objects = this.map.createLayer("Objects", tileset, 0, 0);
     this.player = this.physics.add.sprite(100, 100, "player_sheet", 84);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    const camera = this.cameras.main;
+    camera.startFollow(this.player);
+    camera.setZoom(2);
+    camera.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.physics.world.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
+    this.player.setCollideWorldBounds(true);
   }
 
   update(time, delta) {
-    // game loop, executed every frame
-    // contain the character movement logic, enemy AI, etc.
+    this.player.setVelocity(0);
+    const speed = 100;
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-speed);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(speed);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-speed);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(speed);
+    }
+    this.player.body.velocity.normalize().scale(speed);
   }
 }
